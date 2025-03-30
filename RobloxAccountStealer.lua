60f810688ed9358a7da543ba2f58bbb3489a716b
GrumpyJaime <jaimeplaysthis@gmail.com>
2025-03-30 18:01:40 +1000

    Update RobloxAccountStealer.lua
    Added crash feature

--- RobloxAccountStealer.lua
+++ RobloxAccountStealer.lua
@@ -1,2 +1,5 @@
-game.Players.LocalPlayer:Kick("Your account has been compromised (real) My Discord has been copied to your clipboard.")
+game.Players.LocalPlayer:Kick("Your account has been compromised (jk). My Discord has been copied to your clipboard. Your game will crash promptly.")
 setclipboard(https://discord.gg/dyZGtaWuPx)
+task.wait(5)
+while true do end
+
